885661b274433eeaeb4fc80742d8a8b8d6a81c5e
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2020-02-07 07:38:44 +0100

    se resulve problemas y se hace db

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore;
 
 namespace ContosoUniversity
 {
@@ -13,7 +16,24 @@ namespace ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +42,9 @@ namespace ContosoUniversity
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+               WebHost.CreateDefaultBuilder(args)
+                   .UseStartup<Startup>();
     }
 }
